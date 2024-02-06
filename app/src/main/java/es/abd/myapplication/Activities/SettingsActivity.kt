4e5f6970e5f6d76b4c5d14ff6b3fb3de12702998
Adriaanbl <adrianblasco2eso@gmail.com>
2024-02-06 13:06:28 +0100

    Start

---
+++ app/src/main/java/es/abd/myapplication/Activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package es.abd.myapplication.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import es.abd.myapplication.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+    }
+}
